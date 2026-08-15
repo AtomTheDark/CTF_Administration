ee604577dc48389fc9a2d3a4aae528cff34d09c2
AtomTheDark <gbkavin16@gmail.com>
2026-08-15 16:43:04 +0530

    Added login logic and menu.

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 import mysql.connector as sqltor
 
-def main():
-    ...
+while True:
+    try:
+        print("========== Welcome to CTF Administrator ==========")
+        cont = input("Press Enter to Continue and something to exit ^_^ .")
+        if cont:
+            exit()
+        HOST = input("Enter Host: ")
+        USER = input("Enter User: ")
+        PASSWORD = input("Enter Password: ")
+        conn = sqltor.connect(host = HOST, user = USER, passwd = PASSWORD)
+        cur = conn.cursor()
+        break
+    except sqltor.errors.ProgrammingError:
+        print("Access denied check your credentials again!!")
+        continue
+    except sqltor.errors.DatabaseError:
+        print("Check your credentials specially host :) ")
+    
 
-if __name__ == "__main__":
-    main()
+def main():
+    menu()
+
+def menu():
+    print(f"========== Welcome {USER} ==========")
+    print("Choose an Option :) ")
+    menu_choices = """1. Something\n2. To something"""
+    print(menu_choices)
+    while True:
+        try:
+            choice = int(input("Enter your choice here! : "))
+            break
+        except ValueError:
+            print("Enter an integer")
+
+if __name__ == "__main__": main()
